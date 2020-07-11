5225e843354cd74127ed804789a390228331e3d8
naeluh <naeluh@gmail.com>
2020-07-11 16:29:53 -0700

    Add map

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import dynamic from 'next/dynamic';
+
+const MapWithNoSSR = dynamic(() => import('../components/map'), {
+  ssr: false
+});
 
 function index() {
   return (
     <div>
-      hello
+     <MapWithNoSSR />
     </div>
   )
 }
